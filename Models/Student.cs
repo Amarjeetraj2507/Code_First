196c07bc77bad72304cb9526aaf5c6d183bf5b58
Amarjeet Raj <amarjeetdream@gmail.com>
2023-01-17 19:13:54 +0530

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EF_CodeFirstModel.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Student Name required")]
+        [DisplayName("Student Name")]
+        public string StudentName { get; set; }
+        public string Course { get; set; }
+        public int Age { get; set; }
+    }
+}
